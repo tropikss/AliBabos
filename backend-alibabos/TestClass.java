c3f0e71734fc6d80d1066ffdd16514d25287db2d
mathis <mathis.friess@gmail.com>
2025-10-22 08:49:19 +0200

    test ci

--- backend-alibabos/TestClass.java
+++ backend-alibabos/TestClass.java
@@ -5,5 +5,6 @@ public class TestClass {
         System.out.println("Hello");
         System.out.println('COuCUO');
         System.out.println('csdfqsdf');
+                System.out.println('COuCUO');
     }
 }
