ab7144bfc202711945841c3093275630419b5445
mathis <mathis.friess@gmail.com>
2025-10-22 08:32:36 +0200

    test ci 45345345

--- backend-alibabos/TestClass.java
+++ backend-alibabos/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
         int unusedVariable = 0;
         System.out.println("Hello");
         System.out.println('COuCUO');
+        System.out.println('csdfqsdf');
     }
 }
